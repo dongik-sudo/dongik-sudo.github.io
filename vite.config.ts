1f0347a86680381de16095f253efd793fd408011
dongik-sudo <dongigdong@gmail.com>
2025-09-30 21:37:16 +0900

    why not work?

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/dongik-sudo.github.io",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
