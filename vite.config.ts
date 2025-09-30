e1bfa5e69ccfddb4033c8ca1553975cc6815ccea
dongik-sudo <dongigdong@gmail.com>
2025-09-30 21:41:39 +0900

    pages fixed?

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dongik-sudo.github.io",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/"
 })
